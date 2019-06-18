46b7d68bcec181e18b470a616f81298d8113e26c
Alexandr Semenov <aleksandr.semenov@gs-labs.ru>
2019-06-18 19:13:10 +0200

    Add authorization;

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -13,7 +13,7 @@ pub mod schema;
 pub mod auth;
 
 #[post("/", data = "<credentials>")]
-pub fn login(credentials: Json<UserRequest>, connection: db::Connection) -> Result<Json<JsonValue>, Status> {
+fn login(credentials: Json<UserRequest>, connection: db::Connection) -> Result<Json<JsonValue>, Status> {
     match User::by_username_and_password(credentials.into_inner(), &connection) {
         None => {
             Err(Status::BadRequest)
@@ -26,25 +26,33 @@ pub fn login(credentials: Json<UserRequest>, connection: db::Connection) -> Resu
 }
 
 #[get("/users?<id>")]
-pub fn read(_key: ApiKey, id: Option<i32>, connection: db::Connection) -> Result<Json<JsonValue>, Status> {
+fn read(_key: ApiKey, id: Option<i32>, connection: db::Connection) -> Result<Json<JsonValue>, Status> {
     match User::read(id, &connection) {
         Ok(res) => Ok(Json(json!(res))),
         Err(_) => Err(Status::NotFound)
     }
 }
+#[get("/users?<id>", rank = 2)]
+fn read_error(id: Option<i32>) -> Result<Json<JsonValue>, Status> {
+    Err(Status::Unauthorized)
+}
 
 #[post("/", data = "<user>")]
-pub fn create(user: Json<UserRequest>, connection: db::Connection) -> Result<Json<JsonValue>, Status> {
+fn create(_key: ApiKey, user: Json<UserRequest>, connection: db::Connection) -> Result<Json<JsonValue>, Status> {
     let insert = User { id: None, username: user.username.to_string(), password_hash: user.password.to_string() };
     match User::create(insert, &connection) {
         Ok(res) => Ok(Json(json!(res))),
         Err(_) => Err(Status::BadRequest)
     }
 }
+#[post("/", rank = 2)]
+fn create_error() -> Result<Json<JsonValue>, Status> {
+    Err(Status::Unauthorized)
+}
 
 pub fn mount(rocket: rocket::Rocket) -> rocket::Rocket {
     rocket
-        .mount("/", routes![read])
-        .mount("/user", routes![create])
+        .mount("/", routes![read, read_error])
+        .mount("/user", routes![create, create_error])
         .mount("/login", routes![login])
 }
